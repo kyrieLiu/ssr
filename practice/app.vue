ab45d1fd5687cc5d909043679189f191d8ea38c6
“liuyin” <liuyin@jingcaiwang.cn>
2019-12-16 14:38:06 +0800

    xxx

---
+++ practice/app.vue
@@ -0,0 +1,23 @@
+<!--
+   User: liuyin
+   Date: 2019/12/16 2:10 下午
+   Description:
+ -->
+<template>
+    <div>
+        <p>我是内容</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ practice/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+
+const app = new Vue({
+  template: '<div>this is content</div>'
+})
+
+app.$mount('#root')
